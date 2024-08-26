bed064ff348233e27ef1d6b62bf91ae9dea6b6f8
projectsmartagri <project.smart.agri@gmail.com>
2024-08-26 17:04:11 +0530

    SIGN UP

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:smartagri/modules/farmer/screens/signup_screen.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MaterialApp(home:SignupScreen() ,));
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
-}
+ 
---
+++ lib/modules/farmer/screens/signup_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SignupScreen extends StatelessWidget {
+  const SignupScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("SIGN UP",style: TextStyle(color: Colors.green),),
+        centerTitle: true,
+      ),
+    );
+  }
+}
